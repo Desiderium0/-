515df9a2878d22547c885dd7c550fb0c9226e38f
Desiderium0 <black.cat2222@mail.ru>
2024-09-02 21:00:52 +0800

    код к dll

---
+++ библеотека/Figure.cs
@@ -0,0 +1,42 @@
+namespace Figure
+{
+    public interface IFigure
+    {
+        double CalculaceArea();
+    }
+
+    public class Circle : IFigure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculaceArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : IFigure
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+        public double CalculaceArea()
+        {
+            double x = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(x * (x - SideA) * (x - SideB) * (x - SideC));
+        }
+    }
+}
+
+//Привет тем ктопроверяет код :)
